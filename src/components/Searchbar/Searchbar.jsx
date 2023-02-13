c1f2a4735409b5f1bb154dc73069d706cb5213d3
Natalia <natalakrakoveckaya@gmail.com>
2023-02-12 19:38:04 -0500

    Update SearchBar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'
+import 'react-toastify/dist/ReactToastify.css';
 import { BsSearch } from 'react-icons/bs';
 import {
   SearchbarWrapper,
@@ -10,47 +10,42 @@ import {
   SearchFormInput,
 } from './Searchbar.styled';
 
-
-
-
-export const SearchBar =(onSubmit)=>{
-  const [searchValue, setSearchValue]= useState("");
-   const handleChange = event => {
+export const SearchBar = ({ onSubmit }) => {
+  const [searchValue, setSearchValue] = useState('');
+  const handleChange = event => {
     const { value } = event.target;
     setSearchValue(value.toLowerCase());
   };
 
-
   const handleSubmit = event => {
-        event.preventDefault();
-        if (searchValue.trim() === '') {
-          return toast('Please enter keyword for image search');
-        }
-        onSubmit(searchValue);
-        setSearchValue("");
-      };
-      
-return (
-      <SearchbarWrapper >
-        <SearchForm onSubmit={handleSubmit}>
-          <SearchFormButton type="submit">
-          <BsSearch fill="blue" />
-          </SearchFormButton>
+    event.preventDefault();
+    if (searchValue.trim() === '') {
+      return toast('Please enter keyword for image search');
+    }
+    onSubmit(searchValue);
+    setSearchValue('');
+  };
 
-          <SearchFormInput
-            
-            name="searchValue"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={searchValue}
-            onChange={handleChange}
-          />
-        </SearchForm>
-      </SearchbarWrapper>
-    );
-}
+  return (
+    <SearchbarWrapper>
+      <SearchForm onSubmit={handleSubmit}>
+        <SearchFormButton type="submit">
+          <BsSearch fill="blue" />
+        </SearchFormButton>
+
+        <SearchFormInput
+          name="searchValue"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={searchValue}
+          onChange={handleChange}
+        />
+      </SearchForm>
+    </SearchbarWrapper>
+  );
+};
 
 SearchBar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
